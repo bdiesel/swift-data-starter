ac901fc28ede7da4edb69010689818a352a7a893
Brian Diesel <bri@ndiesel.net>
2025-01-10 14:52:49 -0500

    Initial Commit

---
+++ SwiftDataStarter/SwiftDataStarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataStarterApp.swift
+//  SwiftDataStarter
+//
+//  Created by Brian Diesel on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataStarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftDataStarterTests/SwiftDataStarterTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftDataStarterTests.swift
+//  SwiftDataStarterTests
+//
+//  Created by Brian Diesel on 1/10/25.
+//
+
+import Testing
+
+struct SwiftDataStarterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
