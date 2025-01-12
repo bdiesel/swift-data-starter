6875ce2dad2c16304c5d4614c133925d2015b9c6
Brian Diesel <bri@ndiesel.net>
2025-01-12 17:21:19 -0500

    Adding books to swift data

--- SwiftDataStarter/ContentView.swift
+++ SwiftDataStarter/ContentView.swift
@@ -5,18 +5,34 @@
 //  Created by Brian Diesel on 1/10/25.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query var books: [Book]
+    
+    @State private var showingAddScreen =  false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Text("Count: \(books.count)")
+            List(books) { book in
+                Text(book.title)
+            }
+            
         }
-        .padding()
+        .navigationTitle("Bookworm")
+        .toolbar {
+            Button("Add Book", systemImage: "plus"){
+                showingAddScreen.toggle()
+            }
+        }
+        .sheet(isPresented: $showingAddScreen) {
+               AddBookView()
+           }
     }
+
 }
 
 #Preview {
--- SwiftDataStarter/SwiftDataStarterApp.swift
+++ SwiftDataStarter/SwiftDataStarterApp.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Brian Diesel on 1/10/25.
 //
-
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +13,6 @@ struct SwiftDataStarterApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: Book.self)
     }
 }
